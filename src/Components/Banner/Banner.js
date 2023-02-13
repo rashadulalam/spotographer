77730a7eefae813c5f89eaa2d150242cf7082355
rashadulalam <rashadul91@gmail.com>
2023-02-13 21:48:35 +0600

    Carosel data fetch from json

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,55 +1,41 @@
 import Carousel from 'react-bootstrap/Carousel';
-import Banner1 from '../../images/banner1.jpg';
-import Banner2 from '../../images/banner2.jpg';
-import Banner3 from '../../images/banner3.jpg';
 import { Button } from 'react-bootstrap';
 import './Banner.css';
+import { useEffect, useState } from 'react';
 
 const Banner = () => {
+
+    const [myHero, setMyHero] = useState([]);
+
+    useEffect(() => {
+      fetch("banner.json")
+      .then((res) => res.json())
+      .then((data) => setMyHero(data));
+    }, [])
+
+   
+
     return (
         <>
           <Carousel>
-            <Carousel.Item>
-              <img
-                className="d-block w-100"
-                src={Banner1}
-                alt="First slide"
-              />
-              <Carousel.Caption>
-                <h4>SPOTOGRAPHER</h4>
-                <h3>Sports Photographer</h3>
-                <p>I Capture Sports Moments and Actions.</p>
-                <Button className="sportBtn">More about me</Button>
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-              <img
-                className="d-block w-100"
-                src={Banner2}
-                alt="Second slide"
-              />
 
-              <Carousel.Caption>
-              <h4>SPOTOGRAPHER</h4>
-                <h3>Sports Photographer</h3>
-                <p>I Capture Sports Moments and Actions.</p>
-                <Button className="sportBtn">More about me</Button>
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
+          {
+            myHero.map( (item) => (
+              <Carousel.Item key={item.id}>
               <img
                 className="d-block w-100"
-                src={Banner3}
-                alt="Third slide"
+                src={item.image}
+                alt={item.title}
               />
-
-              <Carousel.Caption>
-                <h4>SPOTOGRAPHER</h4>
-                <h3>Sports Photographer</h3>
-                <p>I Capture Sports Moments and Actions.</p>
-                <Button className="sportBtn">More about me</Button>
-              </Carousel.Caption>
-            </Carousel.Item>
+                <Carousel.Caption>
+                  <h4>{item.subtitle}</h4>
+                  <h3>{item.title}</h3>
+                  <p>{item.txt}</p>
+                  <Button className="sportBtn">More about me</Button>
+                </Carousel.Caption>
+              </Carousel.Item>
+            ))
+          }
           </Carousel>
         </>
     )
